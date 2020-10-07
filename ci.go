255dc90ba5acb952435bfc2b38f00e466875e045
Tsovak <tsovak@bk.ru>
2020-10-07 11:05:23 +0300

    PENV-676: setup workflows

---
+++ ci.go
@@ -0,0 +1,7 @@
+package observerframework
+
+// ci_workflow is the fake code for running github workflow in the empty project.
+// nolint
+func ci_workflow() {
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/insolar/observer-framework
 
 go 1.14
+
+require github.com/ory/go-acc v0.2.3 // indirect
